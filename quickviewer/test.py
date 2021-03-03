be5b59039d411386c969efa0045f5fdc258e8644
H. Pullen <hp346@cam.ac.uk>
2021-03-03 16:05:48 +0000

    removed old files

--- quickviewer/test.py
+++
@@ -1,5 +0,0 @@
-from quickviewer import QuickViewer, ImageViewer, OrthogViewer
-
-in_dir = "~/Work/import/tmp_latest/H01164//MI_BSpline30/"
-#  qv = QuickViewer([in_dir + "ct_relapse.nii", in_dir + "ct_planning.nii"])
-QuickViewer(in_dir + "ct_relapse.nii")
